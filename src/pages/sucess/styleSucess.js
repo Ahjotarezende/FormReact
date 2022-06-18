67ce01e3524d62e965a27f86390f6429a8cf30e7
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-06-18 17:36:44 -0300

    fix: success screen measurement error fixed

--- src/pages/sucess/styleSucess.js
+++ src/pages/sucess/styleSucess.js
@@ -17,10 +17,29 @@ export const Background = styled.div`
     @media screen and (max-width: 768px){
         height: 46rem;
     }
+    @media screen and (max-width: 415px){
+        margin-top: 4rem;
+        padding: 0;
+        height: 100%;
+    }
+    @media screen and (max-width: 376px){
+        margin: 0;
+    }
+    @media screen and (max-height: 812px){
+        margin-top: 4rem;
+        padding: 0;
+        height: 100%;
+    }
+    @media screen and (max-height: 668px){
+        margin-top: 1rem;
+    }
 `;
 
 export const MainTitle = styled.h1`
     font-family: 'Inter', sans-serif;
     margin: 6.93rem 0 8.56rem 0;
     font-size: 2.5rem;
+    @media screen and (max-width: 375px){
+        margin: 4rem 0 7rem 0;
+    }
 `;
